ebe9b87bd5e001bbde5661f732c4b5a9b0750743
manuelarte <manueldoncelmartos@gmail.com>
2026-01-27 15:55:13 +0100

    wip

--- analyzer/testdata/src/equality_comparison/main_test.go
+++ analyzer/testdata/src/equality_comparison/main_test.go
@@ -9,7 +9,7 @@ func TestNewMyStruct(t *testing.T) {
 	t.Parallel()
 
 	want := MyStruct{
-		id: 0,
+		id:   0,
 		name: "John",
 	}
 	got := NewMyStruct(want.id, want.name)
@@ -22,15 +22,15 @@ func TestTableDrivenTestNewMyStruct(t *testing.T) {
 	t.Parallel()
 
 	tests := map[string]struct {
-		id int
+		id   int
 		name string
 		want MyStruct
 	}{
 		"normal values": {
-			id: 0,
+			id:   0,
 			name: "John",
 			want: MyStruct{
-				id: 0,
+				id:   0,
 				name: "John",
 			},
 		},
@@ -38,6 +38,7 @@ func TestTableDrivenTestNewMyStruct(t *testing.T) {
 	for name, tc := range tests {
 		t.Run(name, func(t *testing.T) {
 			got := NewMyStruct(tc.id, tc.name)
+
 			if !reflect.DeepEqual(got, tc.want) {
 				t.Errorf("got %v, want %v", got, tc.want)
 			}
