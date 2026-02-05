cab67fca8ab9c81d4e048f1624a605917d009a0b
manuelarte <manueldoncelmartos@gmail.com>
2026-02-05 23:13:02 +0100

    wip

--- analyzer/testdata/src/failure_message/main_cmp_test.go
+++ analyzer/testdata/src/failure_message/main_cmp_test.go
@@ -27,7 +27,7 @@ func TestCmpEqualSum(t *testing.T) {
 	}
 }
 
-func TestCmpDiff(t *testing.T) {
+func TestCmpDiffWrongFormat(t *testing.T) {
 	t.Parallel()
 
 	want := User{
@@ -38,5 +38,19 @@ func TestCmpDiff(t *testing.T) {
 	got := double(1)
 	if diff := cmp.Diff(got, want); diff != "" {
 		t.Errorf("diff %s", diff) // want `Prefer "diff -want \+got:\\n%s" format for this failure message`
+	}
+}
+
+func TestCmpDiffValidFormat(t *testing.T) {
+	t.Parallel()
+
+	want := User{
+		name:    "John",
+		surname: "Doe",
+		address: Address{},
+	}
+	got := double(1)
+	if diff := cmp.Diff(got, want); diff != "" {
+		t.Errorf("diff -want +got:\n%s", diff)
 	}
 }
