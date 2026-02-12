b2d40d104e9d8dcaafb432fd2f85770cf8211125
manuelarte <manueldoncelmartos@gmail.com>
2026-02-12 16:25:30 +0100

    wip

---
+++ analyzer/testdata/src/failure_message/main_specialcases_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"testing"
+	"time"
+)
+
+type Period struct {
+	StartTime, EndTime time.Time
+}
+
+func (p Period) Duration() time.Duration {
+	return p.EndTime.Sub(p.StartTime)
+}
+
+func TestGetDuration(t *testing.T) {
+	now := time.Now()
+	tests := map[string]struct {
+		period Period
+		want   time.Duration
+	}{
+		"test1": {
+			period: Period{
+				StartTime: now,
+				EndTime:   now.Add(time.Hour),
+			},
+			want: time.Hour,
+		},
+	}
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := test.period.Duration()
+			if got != test.want {
+				t.Errorf("got %v, want %v", got, test.want) // want `Prefer "Duration\(\) = %v, want %v" format for this failure message`
+			}
+		})
+	}
+}
